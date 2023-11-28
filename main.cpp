6c25ac075cdfcae7a16aeba0c9eb4415311d7405
Gaspor <gaspor991@gmail.com>
2023-11-28 18:59:20 -0300

    wip: refactoring code

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 #include <cmath>
 #include <iostream>
 
+// Constantes para melhorar a legibilidade do código
+const float SUN_RADIUS = 2.0f;
+const int NUM_SLICES = 50;
+const int NUM_STACKS = 50;
+const float PLANET_RADIUS[] = {1.0f, 1.5f, 2.0f, 1.8f, 4.0f, 3.5f, 2.5f, 2.3f, 1.2f};
+const float PLANET_DISTANCE[] = {5.0f, 8.0f, 11.0f, 15.0f, 22.0f, 30.0f, 40.0f, 45.0f, 50.0f};
+
 float cameraX = 0.0f;
 float cameraY = 0.0f;
 float cameraZ = 30.0f;
@@ -18,233 +25,148 @@ bool isLeftButtonPressed = false;
 int prevMouseX = 0;
 int prevMouseY = 0;
 
-void setupLighting() {
+void writeplanetName(const std::string& planetName) {
+    for (char c : planetName) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
+    }
+}
+
+void drawSun() {
+    // Configuração de iluminação para o Sol
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
 
-    // Posição da luz pontual
-    GLfloat lightPos[] = { 0.0f, 0.0f, 0.0f, 1.0f };
-    glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
-
-    // Parâmetros de atenuação (opcional, dependendo da aparência desejada)
-    GLfloat attenuation[] = { 1.0f, 0.0f, 0.0f };
-    glLightfv(GL_LIGHT0, GL_CONSTANT_ATTENUATION, attenuation);
-
-    // Cor da luz pontual
-    GLfloat lightColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+    GLfloat lightPosition[] = {0.0f, 0.0f, 0.0f, 1.0f}; // Luz direcional do Sol
+    GLfloat lightColor[] = {1.0f, 1.0f, 0.0f, 1.0f};     // Cor amarela
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor);
+
+    // Desenha o Sol
+    glColor3f(1.0f, 1.0f, 0.0f); // Cor amarela
+    for (int i = 0; i < NUM_STACKS; ++i) {
+        float theta1 = i * (M_PI / NUM_STACKS);
+        float theta2 = (i + 1) * (M_PI / NUM_STACKS);
+
+        glBegin(GL_TRIANGLE_STRIP);
+        for (int j = 0; j <= NUM_SLICES; ++j) {
+            float phi = j * (2.0f * M_PI / NUM_SLICES);
+            float x1 = SUN_RADIUS * sin(theta1) * cos(phi);
+            float y1 = SUN_RADIUS * sin(theta1) * sin(phi);
+            float z1 = SUN_RADIUS * cos(theta1);
+
+            float x2 = SUN_RADIUS * sin(theta2) * cos(phi);
+            float y2 = SUN_RADIUS * sin(theta2) * sin(phi);
+            float z2 = SUN_RADIUS * cos(theta2);
+
+            glNormal3f(x1, y1, z1);
+            glVertex3f(x1, y1, z1);
+
+            glNormal3f(x2, y2, z2);
+            glVertex3f(x2, y2, z2);
+        }
+        glEnd();
+    }
+
+    glRasterPos3f(1.5f, 2.5f, 0.0f);
+    writeplanetName("Sol");
+
+    glDisable(GL_LIGHT0);
+    glDisable(GL_LIGHTING);
 }
 
-void renderScene()
-{
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    setupLighting();
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    gluLookAt(cameraX, cameraY, cameraZ, cameraX, cameraY, cameraZ - 1.0, 0.0, 1.0, 0.0);
-
-    glColor3f(1.0f, 1.0f, 0.0f); // Cor amarela
-    glutSolidSphere(2.0, 20, 20);
-    glRasterPos3f(1.5f, 1.5f, 0.0f);
-    std::string sunName = "Sol";
-    for (char c : sunName)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
-    }
-
-    glPushMatrix();
-    glRotatef(angle, 0.0f, 1.0f, 0.0f);
-
-    // Mercúrio
-    glPushMatrix();
-    glRotatef(angle * 2.0f, 0.0f, 1.0f, 0.0f);
-    glTranslatef(5.0f, 0.0f, 0.0f);
-    glColor3f(0.1f, 0.1f, 0.1f); // Cor cinza escuro
-    glutSolidSphere(1.0, 20, 20);
-    glRasterPos3f(1.5f, 1.5f, 0.0f);
-    std::string planet1Name = "Mercurio";
-    for (char c : planet1Name)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
-    }
-    glPopMatrix();
-
-    // Vênus
-    glPushMatrix();
-    glRotatef(angle * 1.5f, 0.0f, 1.0f, 0.0f);
-    glTranslatef(8.0f, 0.0f, 0.0f);
-    glColor3f(1.0f, 0.0f, 0.0f); // Cor vermelha
-    glutSolidSphere(1.5, 20, 20);
-    glRasterPos3f(1.5f, 1.5f, 0.0f);
-    std::string planet2Name = "Venus";
-    for (char c : planet2Name)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
-    }
-    glPopMatrix();
-
-    // Terra
-    glPushMatrix();
-    glRotatef(angle, 0.0f, 1.0f, 0.0f);
-    glTranslatef(11.0f, 0.0f, 0.0f);
-    glColor3f(0.0f, 0.0f, 1.0f); // Cor azul
-    glutSolidSphere(2.0, 20, 20);
-    glRasterPos3f(1.5f, 2.5f, 0.0f);
-    std::string planet3Name = "Terra";
-    for (char c : planet3Name)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
-    }
-    glPopMatrix();
-
-    // Marte
-    glPushMatrix();
-    glRotatef(angle * 0.8f, 0.0f, 1.0f, 0.0f);
-    glTranslatef(15.0f, 0.0f, 0.0f);
-    glColor3f(1.0f, 0.5f, 0.0f); // Cor laranja
-    glutSolidSphere(1.8, 20, 20);
-    glRasterPos3f(1.5f, 2.5f, 0.0f);
-    std::string planet4Name = "Marte";
-    for (char c : planet4Name)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
-    }
-    glPopMatrix();
-
-    // Júpiter
-    glPushMatrix();
-    glRotatef(angle * 0.6f, 0.0f, 1.0f, 0.0f);
-    glTranslatef(22.0f, 0.0f, 0.0f);
-    glColor3f(0.1f, 0.2f, 0.0f); // Cor verde musgo
-    glutSolidSphere(4.0, 20, 20);
-    glRasterPos3f(1.5f, 4.5f, 0.0f);
-    std::string planet5Name = "Jupiter";
-    for (char c : planet5Name)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
-    }
-    glPopMatrix();
-
-    // Saturno
-    glPushMatrix();
-    glRotatef(angle * 0.4f, 0.0f, 1.0f, 0.0f);
-    glTranslatef(30.0f, 0.0f, 0.0f);
-    glColor3f(1.0f, 1.0f, 0.0f); // Cor amarela
-    glutSolidSphere(3.5, 20, 20);
-
+void drawSaturnRings() {
+    // Anéis de Saturno
     glColor3f(0.5f, 0.5f, 0.5f); // Cor cinza
     glRotatef(30.0f, 1.0f, 0.0f, 0.0f);
     glScalef(1.0f, 1.0f, 0.05f);
     glutSolidTorus(1.5, 7.0, 20, 20);
-    glRasterPos3f(1.5f, 3.5f, 0.0f);
-    std::string planet6Name = "Saturno";
-    for (char c : planet6Name)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
-    }
-    glPopMatrix();
+}
 
-    // Urano
+void drawPlanet(float radius, float distance, float colorR, float colorG, float colorB, const std::string& planetName, bool hasRings = false) {
     glPushMatrix();
-    glRotatef(angle * 0.3f, 0.0f, 1.0f, 0.0f);
-    glTranslatef(40.0f, 0.0f, 0.0f);
-    glColor3f(0.0f, 1.0f, 1.0f); // Cor ciano
-    glutSolidSphere(2.5, 20, 20);
+    glRotatef(angle * (5.0f / distance), 0.0f, 1.0f, 0.0f);
+    glTranslatef(distance, 0.0f, 0.0f);
+    glColor3f(colorR, colorG, colorB);
+    glutSolidSphere(radius, 20, 20);
     glRasterPos3f(1.5f, 2.5f, 0.0f);
-    std::string planet7Name = "Urano";
-    for (char c : planet7Name)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
+    writeplanetName(planetName);
+    if (hasRings) {
+        drawSaturnRings();
     }
     glPopMatrix();
+}
 
-    // Netuno
-    glPushMatrix();
-    glRotatef(angle * 0.2f, 0.0f, 1.0f, 0.0f);
-    glTranslatef(45.0f, 0.0f, 0.0f);
-    glColor3f(0.0f, 0.0f, 0.5f); // Cor azul escuro
-    glutSolidSphere(2.3, 20, 20);
-    glRasterPos3f(1.5f, 2.5f, 0.0f);
-    std::string planet8Name = "Netuno";
-    for (char c : planet8Name)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
-    }
-    glPopMatrix();
+void renderScene() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Plutão
-    glPushMatrix();
-    glRotatef(angle * 0.1f, 0.0f, 1.0f, 0.0f);
-    glTranslatef(50.0f, 0.0f, 0.0f);
-    glColor3f(0.5f, 0.0f, 0.5f); // Cor roxa
-    glutSolidSphere(1.2, 20, 20);
-    glRasterPos3f(1.5f, 1.5f, 0.0f);
-    std::string planet9Name = "Plutao";
-    for (char c : planet9Name)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
-    }
-    glPopMatrix();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 
-    glPopMatrix();
+    glTranslatef(0.0f, 0.0f, -cameraZ);
+    glRotatef(cameraAngleX, 1.0f, 0.0f, 0.0f);
+    glRotatef(cameraAngleY, 0.0f, 1.0f, 0.0f);
+    glTranslatef(cameraX, cameraY, cameraZ);
+
+    gluLookAt(cameraX, cameraY, cameraZ, cameraX, cameraY, cameraZ - 1.0, 0.0, 1.0, 0.0);
+
+    // Sol
+    drawSun();
+
+    // Planetas
+    drawPlanet(1.0f, PLANET_DISTANCE[0], 0.1f, 0.1f, 0.1f, "Mercurio");
+    drawPlanet(1.5f, PLANET_DISTANCE[1], 1.0f, 0.0f, 0.0f, "Venus");
+    drawPlanet(2.0f, PLANET_DISTANCE[2], 0.0f, 0.0f, 1.0f, "Terra");
+    drawPlanet(1.8f, PLANET_DISTANCE[3], 1.0f, 0.5f, 0.0f, "Marte");
+    drawPlanet(4.0f, PLANET_DISTANCE[4], 0.1f, 0.2f, 0.0f, "Jupiter");
+    drawPlanet(3.5f, PLANET_DISTANCE[5], 1.0f, 1.0f, 0.0f, "Saturno", true);
+    drawPlanet(2.5f, PLANET_DISTANCE[6], 0.0f, 1.0f, 1.0f, "Urano");
+    drawPlanet(2.3f, PLANET_DISTANCE[7], 0.0f, 0.0f, 0.5f, "Netuno");
+    drawPlanet(1.2f, PLANET_DISTANCE[8], 0.5f, 0.0f, 0.5f, "Plutao");
 
     glutSwapBuffers();
 }
 
-void updateScene()
-{
+void updateScene() {
     angle += 0.01f;
     glutPostRedisplay();
 }
 
-void processUserInput(unsigned char key, int x, int y)
-{
-    switch (key)
-    {
-    case 'w':
-        cameraZ -= cameraSpeed;
-        break;
-    case 's':
-        cameraZ += cameraSpeed;
-        break;
-    case 'a':
-        cameraX -= cameraSpeed;
-        break;
-    case 'd':
-        cameraX += cameraSpeed;
-        break;
-    case 'q':
-        exit(0);
-        break;
+void processUserInput(unsigned char key, int x, int y) {
+    switch (key) {
+        case 'w':
+            cameraZ -= cameraSpeed;
+            break;
+        case 's':
+            cameraZ += cameraSpeed;
+            break;
+        case 'a':
+            cameraX -= cameraSpeed;
+            break;
+        case 'd':
+            cameraX += cameraSpeed;
+            break;
+        case 'q':
+            exit(0);
+            break;
     }
 
     glutPostRedisplay();
 }
 
-void processMouse(int button, int state, int x, int y)
-{
-    if (button == GLUT_LEFT_BUTTON)
-    {
-        if (state == GLUT_DOWN)
-        {
+void processMouse(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON) {
+        if (state == GLUT_DOWN) {
             isLeftButtonPressed = true;
             prevMouseX = x;
             prevMouseY = y;
-        }
-        else if (state == GLUT_UP)
-        {
+        } else if (state == GLUT_UP) {
             isLeftButtonPressed = false;
         }
     }
 }
 
-void processMouseMotion(int x, int y)
-{
-    if (isLeftButtonPressed)
-    {
+void processMouseMotion(int x, int y) {
+    if (isLeftButtonPressed) {
         int deltaX = x - prevMouseX;
         int deltaY = y - prevMouseY;
         prevMouseX = x;
@@ -258,8 +180,8 @@ void processMouseMotion(int x, int y)
         else if (cameraAngleY < 0.0f)
             cameraAngleY += 360.0f;
 
-        float angleYRad = cameraAngleY * 3.1415926535f / 180.0f;
-        float angleXRad = cameraAngleX * 3.1415926535f / 180.0f;
+        float angleYRad = cameraAngleY * M_PI / 180.0f;
+        float angleXRad = cameraAngleX * M_PI / 180.0f;
 
         cameraX = cameraZ * sin(angleYRad) * cos(angleXRad);
         cameraY = cameraZ * sin(angleXRad);
@@ -268,8 +190,7 @@ void processMouseMotion(int x, int y)
     }
 }
 
-void reshape(int width, int height)
-{
+void reshape(int width, int height) {
     glViewport(0, 0, width, height);
 
     glMatrixMode(GL_PROJECTION);
@@ -277,12 +198,12 @@ void reshape(int width, int height)
     gluPerspective(45.0f, (float)width / height, 0.1f, 1000.0f);
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     std::cout << "w: Movimentar a camera para frente\n";
     std::cout << "s: Movimentar a camera para tras\n";
     std::cout << "a: Movimentar a camera para a esquerda\n";
     std::cout << "d: Movimentar a camera para a direita\n";
+    std::cout << "Mouse (Botao esquerdo): Fazer rotação em relação ao sol\n";
     std::cout << "q: Sair do programa\n";
 
     glutInit(&argc, argv);
